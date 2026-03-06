9ea9970c1b099b92516bf3bd2c7234a2f643a329
zakiz06 <zakizrka@gmail.com>
2026-03-06 10:06:27 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,11 +1,9 @@
-<!doctype html>
 <html lang="fr">
 
     <head>
         <meta charset="utf-8">
         <title>Accueil Ticketing service</title>
         <link rel="stylesheet" href="index.css">
-        <script src="index.js"></script>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.0/font/bootstrap-icons.css">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
@@ -53,11 +51,49 @@
         <div class="container-lg">
             <hr>
             <h2>New Ticket</h2>
-
         </div>
-        
-        
-            
+
+
+
+        <?php
+
+            $servername = 'localhost';
+            $username = 'admin';
+            $password = 'admin';
+            $bdd = 'Ticketing';
+            $table = 'tickets';
+
+            $db = mysqli_connect($servername, $username, $password, $bdd);
+
+           function dbquery(string $query){
+            global $db;
+             $result = mysqli_query($db, $query);
+            if (!$result) {
+             echo 'query pas ok';
+            }
+             return $result;
+            }
+
+function ticketsDisplay(){
+    $sql = "SELECT * FROM tickets";
+    $result = dbquery($sql);
+
+    echo '<table border="1">';
+    echo '<tr><th>ID</th><th>Titre</th><th>Description</th></tr>';
+
+    while ($row = mysqli_fetch_assoc($result)) {
+        echo '<tr>';
+        echo '<td>'.$row['id'].'</td>';
+        echo '<td>'.$row['full_name'].'</td>';
+        echo '<td>'.$row['title'].'</td>';
+        echo '<td>'.$row['msg'].'</td>';
+        echo '<td>'.$row['priority'].'</td>';
+        echo '</tr>';
+    }
+    echo '</table>';
+}
+            ticketsDisplay();
+?>            
 
     </body>
 
